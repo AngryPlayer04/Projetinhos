f164fb0f90a7ad6c33d646cc7b02a9d497fd47f0
Cássio Nhiemetz <60411891+AngryPlayer04@users.noreply.github.com>
2022-08-09 14:31:47 -0300

    funcionando

---
+++ Física/altura.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+print('Olá, bem vindo ao calculador de altura, baseado na fóruma h=gt²/2')
+
+tempo = print('Qual o tempo, em segundos? ')
+
+g = 10
+t = int(input())**2
+h = (g * t)/2
+print(f'Sendo a gravidade 10m/s², altura é {h}m')
